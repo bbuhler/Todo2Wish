60915ac9a2e5d12f224880b475fa74501b9b519e
bbuhler <github@freiken-douhl.de>
2019-05-31 18:15:06 +0200

    Refactored WishListView and NewWishView

--- lib/views/NewWishView.dart
+++ lib/views/NewWishView.dart
@@ -4,28 +4,73 @@ class NewWishView extends StatelessWidget {
   NewWishView({Key key, this.onCreate}) : super(key: key);
 
   final Function onCreate;
+  final wishTitleCtrl = TextEditingController();
+  final wishValueCtrl = TextEditingController();
+
+  String numberValidator(String value) {
+    if (value == null) {
+      return null;
+    }
+    final n = int.tryParse(value);
+    if (n == null) {
+      return '"$value" is not a valid integer';
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
+    Color bottomAppBarColor = Theme.of(context).bottomAppBarColor;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Add a wish'),
-      ),
-      body: TextField(
-        // TODO use a Form
-        // TODO add icon before
-        // TODO floating label?
-        // TODO Add price category
-        autofocus: true,
-        onSubmitted: (val) {
-          onCreate(val);
-          Navigator.pop(context);
-        },
-        decoration: InputDecoration(
-          hintText: 'Enter a wish...',
-          contentPadding: const EdgeInsets.all(16.0),
+        appBar: AppBar(
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.check, color: bottomAppBarColor),
+              onPressed: () {
+                onCreate(wishTitleCtrl.text, int.tryParse(wishValueCtrl.text));
+                Navigator.pop(context);
+              },
+            ),
+          ],
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(80.0),
+            child: TextField(
+              autofocus: true,
+              cursorColor: bottomAppBarColor,
+              controller: wishTitleCtrl,
+              style: TextStyle(
+                fontSize: 24.0,
+                color: bottomAppBarColor,
+              ),
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.check_box_outline_blank,
+                  color: bottomAppBarColor,
+                ),
+                contentPadding: const EdgeInsets.all(20.0),
+                hintText: 'Enter a wish...',
+                hintStyle: TextStyle(color: bottomAppBarColor.withOpacity(.75)),
+              ),
+            ),
+          ),
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.only(top: 10.0),
+          child: Column(children: <Widget>[
+            TextFormField(
+              controller: wishValueCtrl,
+              keyboardType: TextInputType.number,
+              validator: numberValidator,
+              inputFormatters: [],
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.stars),
+                contentPadding: const EdgeInsets.all(16.0),
+                hintText: 'Wish value',
+                border: InputBorder.none,
+              ),
+            ),
+          ]),
+        ));
   }
 }
--- lib/views/WishListView.dart
+++ lib/views/WishListView.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:todo2wish/models/DataProvider.dart';
 import 'package:todo2wish/views/BaseList.dart';
@@ -15,27 +17,129 @@ class WishList extends StatefulWidget {
 class WishListState extends State<WishList> {
   List<Todo> _wishes;
 
-  void loadWishesFromDb() async {
-    List wishes = await widget.db.getTodos(TodoType.wish);
+  void reloadFromDb() async {
+    List<Todo> wishes = await widget.db.getTodos(TodoType.wish);
     setState(() => _wishes = wishes);
   }
 
   @override
   void initState() {
-    this.loadWishesFromDb();
+    reloadFromDb();
     super.initState();
   }
 
-  void _addWishItem(String wish) async {
-    if (wish.length > 0) {
+  void _showAddItemScreen() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: NewWishView(
+          onCreate: _addItem,
+        ).build,
+      ),
+    );
+  }
+
+  void _addItem(String wish, int value) async {
+    if (wish.length > 0 && value != null) {
       Todo todo = Todo();
       todo.type = TodoType.wish;
       todo.title = wish;
-      todo.value = -3;
+      todo.value = value * -1;
       todo.since = DateTime.now();
       await widget.db.insertTodo(todo);
-      this.loadWishesFromDb();
+      reloadFromDb();
     }
+  }
+
+  Future<bool> _isToggleDoneAllowed(Todo wish) async {
+    return wish.done == null &&
+        widget.db.balance.value >= wish.value.abs() &&
+        await _toggleItemConfirm(wish, 'FULFILL');
+  }
+
+  Future<bool> _isToggleUndoneAllowed(Todo wish) async {
+    return wish.done != null && await _toggleItemConfirm(wish, 'UNFULFILLED');
+  }
+
+  Future _toggleItem(Todo wish) async {
+    if (await _isToggleDoneAllowed(wish)) {
+      wish.done = DateTime.now();
+      widget.db.updateTodo(wish);
+    } else if (await _isToggleUndoneAllowed(wish)) {
+      wish.done = null;
+      widget.db.updateTodo(wish);
+    }
+
+    widget.db.fetchBalance();
+    reloadFromDb();
+  }
+
+  Future<bool> _toggleItemConfirm(Todo wish, String action) {
+    Completer<bool> completer = Completer();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(wish.title),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('CANCEL'),
+              onPressed: () {
+                completer.complete(false);
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text(action),
+              onPressed: () {
+                completer.complete(true);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    return completer.future;
+  }
+
+  void _removeItem(Todo wish) async {
+    if (wish.done == null && await _removeItemConfirm(wish)) {
+      await widget.db.deleteTodo(wish.id, TodoType.wish);
+      this.reloadFromDb();
+    }
+  }
+
+  Future<bool> _removeItemConfirm(Todo wish) async {
+    Completer<bool> completer = Completer();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Delete entry "${wish.title}"?'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('CANCEL'),
+              onPressed: () {
+                completer.complete(false);
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text('DELETE'),
+              onPressed: () {
+                completer.complete(true);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    return completer.future;
   }
 
   @override
@@ -45,59 +149,9 @@ class WishListState extends State<WishList> {
       openTitle: Text('WISHES'),
       doneTitle: Text('DONE'),
       valueStyle: TextStyle(color: Colors.green, fontSize: 16.0),
-      onAddItem: _pushAddWishScreen,
-      onToggleItem: _toggleWishItem,
-      onDeleteItem: _promptRemoveWishItem,
-    );
-  }
-
-  void _pushAddWishScreen() {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: NewWishView(
-          onCreate: _addWishItem,
-        ).build,
-      ),
-    );
-  }
-
-  void _removeWishItem(Todo wish) async {
-    await widget.db.deleteTodo(wish.id, TodoType.wish);
-    this.loadWishesFromDb();
-  }
-
-  void _toggleWishItem(Todo wish) {
-    if (wish.done == null) {
-      wish.done = DateTime.now();
-    } else {
-      wish.done = null;
-    }
-    widget.db.updateTodo(wish);
-    widget.db.fetchBalance();
-    this.loadWishesFromDb();
-  }
-
-  void _promptRemoveWishItem(Todo wish) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Delete entry "${wish.title}"?'),
-          actions: <Widget>[
-            FlatButton(
-              child: Text('CANCEL'),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-            FlatButton(
-              child: Text('DELETE'),
-              onPressed: () {
-                _removeWishItem(wish);
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
+      onAddItem: _showAddItemScreen,
+      onToggleItem: _toggleItem,
+      onDeleteItem: _removeItem,
     );
   }
 }
